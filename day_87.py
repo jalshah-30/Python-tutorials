be7d9f4389a4bd1c5cd731029f9a5177992e985b
jalshah-30 <jalshahpcps9thb@gmail.com>
2025-08-30 23:08:30 +0530

    Day 87 (Shutil)

---
+++ day_87.py
@@ -0,0 +1,7 @@
+'''Shutil Module in Python'''
+import shutil
+shutil.copy("day_87.py","day_87_.py")
+
+#shutil.copytree is used for copying folders
+#shutil.move moves the file
+#shutil.rmtree helps for deleting a file
